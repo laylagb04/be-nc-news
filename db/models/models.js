b6a52a7e6ebc3dc2da090816d248d1580712560a
Layla Broadhead <laylabroadhead@laylas-mbp.home>
2024-10-14 16:41:03 +0100

    added function get article by id

--- db/models/models.js
+++ db/models/models.js
@@ -4,4 +4,15 @@ const fetchTopics = () => {
   return db.query(`SELECT * FROM topics`);
 };
 
-module.exports = { fetchTopics };
+const fetchArticlesById = (article_id) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .then((response) => {
+      if (response.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "article does not exist" });
+      }
+      return response.rows[0];
+    });
+};
+
+module.exports = { fetchTopics, fetchArticlesById };
