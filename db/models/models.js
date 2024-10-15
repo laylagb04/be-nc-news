5cdcb18fb6e074f7810942b9e266953787b27d2e
Layla Broadhead <laylabroadhead@Laylas-MacBook-Pro.local>
2024-10-15 13:40:57 +0100

    fetchArticles added

--- db/models/models.js
+++ db/models/models.js
@@ -15,4 +15,23 @@ const fetchArticlesById = (article_id) => {
     });
 };
 
-module.exports = { fetchTopics, fetchArticlesById };
+const fetchArticles = () => {
+  return db.query(`SELECT 
+    articles.article_id,
+    articles.title,
+    articles.topic,
+    articles.author,
+    articles.created_at,
+    articles.votes,
+    articles.article_img_url,
+    COUNT(comments.comment_id) AS comment_count
+    FROM 
+    articles 
+    LEFT JOIN 
+    comments
+    on articles.article_id = comments.article_id
+    GROUP BY articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.article_img_url
+    ORDER BY created_at DESC;`);
+};
+
+module.exports = { fetchTopics, fetchArticlesById, fetchArticles };
